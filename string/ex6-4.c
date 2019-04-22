ca08ab81ea2739bec41c301cea07b71e7594a70e
Bertone-Porto <38756105+Bertone-Porto@users.noreply.github.com>
2019-04-22 16:53:03 -0300

    Create ex6-4.c

---
+++ string/ex6-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void strlen2(char* s1){ //retorna a qtd de caracteres
+  int i=0, cont=0;
+  do{
+    cont += 1;
+    i++;
+  }while(s1[i] != '\0' );
+  printf("Quantidade de letras: %d\n", cont);
+}
+
+
+int main(void) {
+  char s1[15];
+  printf("digite uma string: \n");
+  scanf("%s", s1);
+  strlen2(s1);
+
+  return 0;
+}
